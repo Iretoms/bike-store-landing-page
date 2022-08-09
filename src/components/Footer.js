853e2f2518143972aecca4605d2f57d53436343e
iretoms <iretomiwajonathan@gmail.com>
2022-08-08 19:21:00 -0700

    Footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,100 @@
+import React from "react";
+import styled from "styled-components";
+import Logo from "./Logo";
+
+const Footer = () => {
+  return (
+    <MainFooter>
+      <Container>
+        <FooterText>
+          <div className="logo-container">
+            <Logo />
+          </div>
+          <p >
+            Trafalgar provides progressive, and affordable healthcare,
+            accessible on mobile and online for everyone
+          </p>
+          <p>&copy;eBike 2021. All rights reserved</p>
+        </FooterText>
+        <FooterLink>
+          <ul>
+            <li>Company</li>
+            <li>Product</li>
+            <li>Bike Type</li>
+            <li>About Us</li>
+            <li>Contact</li>
+          </ul>
+          <ul>
+            <li>Help</li>
+            <li>Help center</li>
+            <li>Contact support</li>
+            <li>Instructions</li>
+            <li>How it works</li>
+          </ul>
+        </FooterLink>
+      </Container>
+    </MainFooter>
+  );
+};
+
+const MainFooter = styled.footer`
+  width: 100%;
+  min-height: 70vh;
+  background: #fcb72b;
+  display: flex;
+  align-items: center;
+`;
+
+const Container = styled.div`
+color: #fff;
+width: 70%;
+margin: 0 auto;
+display: flex;
+justify-content: space-between;
+article {
+    width:40%;
+}
+`;
+
+const FooterText = styled.article`
+  .logo-container {
+    margin-bottom: 1rem;
+    .big-B {
+      background-color: #fff;
+      color: #ffbd37;
+    }
+    .e-bike {
+        color: #fff;
+    }
+  }
+  p:nth-of-type(1){
+    font-weight: 300;
+    font-size: 1.1rem;
+    margin-bottom: 2rem;
+  }
+  p:nth-of-type(2){
+    font-weight: 300;
+    font-size: 1rem;
+  }
+`;
+
+const FooterLink = styled.article`
+display: flex;
+justify-content: space-between;
+ul {
+    list-style: none;
+    li {
+        font-weight: 300;
+        font-size: 1.1rem;
+        margin-bottom: 1rem;
+    }
+
+    li:nth-of-type(1){
+        font-weight: 700;
+        font-size: 1.2rem;
+        margin-bottom: 1.5rem;
+    }
+}
+`
+
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BikeTypes from '../components/BikeTypes';
 import Customer from '../components/Customer';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import HybridBikes from '../components/HybridBikes';
@@ -12,7 +13,8 @@ const Home = () => {
          <Hero/>
          <BikeTypes/>
          <HybridBikes/>
-         <Customer/>   
+         <Customer/>
+         <Footer/>  
         </>
     );
 };
